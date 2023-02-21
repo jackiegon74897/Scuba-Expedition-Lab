b41bb5d29120ea5f27d9f9d7e337b72ce2235f2f
TylGa <TylGa@TYL04GARPC>
2023-02-21 02:38:24 -0800

    Add project files.

---
+++ Scuba Expedition/frmScuba.Designer.vb
@@ -0,0 +1,211 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmScuba
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pnlScuba = New System.Windows.Forms.Panel()
+        Me.lblLength = New System.Windows.Forms.Label()
+        Me.lblScubaDive = New System.Windows.Forms.Label()
+        Me.lblCost = New System.Windows.Forms.Label()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnDiveCost = New System.Windows.Forms.Button()
+        Me.lstDive = New System.Windows.Forms.ListBox()
+        Me.lblSelect = New System.Windows.Forms.Label()
+        Me.txtTeam = New System.Windows.Forms.TextBox()
+        Me.lblTeam = New System.Windows.Forms.Label()
+        Me.cboComboBox = New System.Windows.Forms.ComboBox()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.pnlScuba.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlScuba
+        '
+        Me.pnlScuba.BackColor = System.Drawing.Color.Transparent
+        Me.pnlScuba.Controls.Add(Me.lblLength)
+        Me.pnlScuba.Controls.Add(Me.lblScubaDive)
+        Me.pnlScuba.Controls.Add(Me.lblCost)
+        Me.pnlScuba.Controls.Add(Me.btnClear)
+        Me.pnlScuba.Controls.Add(Me.btnDiveCost)
+        Me.pnlScuba.Controls.Add(Me.lstDive)
+        Me.pnlScuba.Controls.Add(Me.lblSelect)
+        Me.pnlScuba.Controls.Add(Me.txtTeam)
+        Me.pnlScuba.Controls.Add(Me.lblTeam)
+        Me.pnlScuba.Controls.Add(Me.cboComboBox)
+        Me.pnlScuba.Controls.Add(Me.lblTitle)
+        Me.pnlScuba.Location = New System.Drawing.Point(435, 1)
+        Me.pnlScuba.Name = "pnlScuba"
+        Me.pnlScuba.Size = New System.Drawing.Size(414, 438)
+        Me.pnlScuba.TabIndex = 0
+        '
+        'lblLength
+        '
+        Me.lblLength.AutoSize = True
+        Me.lblLength.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLength.Location = New System.Drawing.Point(107, 392)
+        Me.lblLength.Name = "lblLength"
+        Me.lblLength.Size = New System.Drawing.Size(242, 32)
+        Me.lblLength.TabIndex = 9
+        Me.lblLength.Text = "XXXXXXXXXXXX"
+        Me.lblLength.Visible = False
+        '
+        'lblScubaDive
+        '
+        Me.lblScubaDive.AutoSize = True
+        Me.lblScubaDive.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScubaDive.Location = New System.Drawing.Point(107, 312)
+        Me.lblScubaDive.Name = "lblScubaDive"
+        Me.lblScubaDive.Size = New System.Drawing.Size(242, 32)
+        Me.lblScubaDive.TabIndex = 8
+        Me.lblScubaDive.Text = "XXXXXXXXXXXX"
+        Me.lblScubaDive.Visible = False
+        '
+        'lblCost
+        '
+        Me.lblCost.AutoSize = True
+        Me.lblCost.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCost.Location = New System.Drawing.Point(107, 354)
+        Me.lblCost.Name = "lblCost"
+        Me.lblCost.Size = New System.Drawing.Size(242, 32)
+        Me.lblCost.TabIndex = 7
+        Me.lblCost.Text = "XXXXXXXXXXXX"
+        Me.lblCost.Visible = False
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnClear.ForeColor = System.Drawing.Color.White
+        Me.btnClear.Location = New System.Drawing.Point(223, 262)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(100, 34)
+        Me.btnClear.TabIndex = 6
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        Me.btnClear.Visible = False
+        '
+        'btnDiveCost
+        '
+        Me.btnDiveCost.BackColor = System.Drawing.Color.MidnightBlue
+        Me.btnDiveCost.ForeColor = System.Drawing.Color.White
+        Me.btnDiveCost.Location = New System.Drawing.Point(104, 262)
+        Me.btnDiveCost.Name = "btnDiveCost"
+        Me.btnDiveCost.Size = New System.Drawing.Size(100, 34)
+        Me.btnDiveCost.TabIndex = 5
+        Me.btnDiveCost.Text = "Dive Cost"
+        Me.btnDiveCost.UseVisualStyleBackColor = False
+        Me.btnDiveCost.Visible = False
+        '
+        'lstDive
+        '
+        Me.lstDive.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstDive.FormattingEnabled = True
+        Me.lstDive.ItemHeight = 32
+        Me.lstDive.Location = New System.Drawing.Point(123, 188)
+        Me.lstDive.Name = "lstDive"
+        Me.lstDive.Size = New System.Drawing.Size(200, 68)
+        Me.lstDive.TabIndex = 4
+        Me.lstDive.Visible = False
+        '
+        'lblSelect
+        '
+        Me.lblSelect.AutoSize = True
+        Me.lblSelect.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSelect.Location = New System.Drawing.Point(98, 141)
+        Me.lblSelect.Name = "lblSelect"
+        Me.lblSelect.Size = New System.Drawing.Size(246, 32)
+        Me.lblSelect.TabIndex = 1
+        Me.lblSelect.Text = "Select Scuba Dive"
+        Me.lblSelect.Visible = False
+        '
+        'txtTeam
+        '
+        Me.txtTeam.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTeam.Location = New System.Drawing.Point(311, 90)
+        Me.txtTeam.MaxLength = 2
+        Me.txtTeam.Name = "txtTeam"
+        Me.txtTeam.Size = New System.Drawing.Size(67, 39)
+        Me.txtTeam.TabIndex = 3
+        Me.txtTeam.Visible = False
+        '
+        'lblTeam
+        '
+        Me.lblTeam.AutoSize = True
+        Me.lblTeam.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTeam.Location = New System.Drawing.Point(3, 97)
+        Me.lblTeam.Name = "lblTeam"
+        Me.lblTeam.Size = New System.Drawing.Size(287, 32)
+        Me.lblTeam.TabIndex = 2
+        Me.lblTeam.Text = "Number in Dive Team"
+        Me.lblTeam.Visible = False
+        '
+        'cboComboBox
+        '
+        Me.cboComboBox.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboComboBox.FormattingEnabled = True
+        Me.cboComboBox.Items.AddRange(New Object() {"Australia", "Belize", "Fiji"})
+        Me.cboComboBox.Location = New System.Drawing.Point(113, 44)
+        Me.cboComboBox.Name = "cboComboBox"
+        Me.cboComboBox.Size = New System.Drawing.Size(232, 40)
+        Me.cboComboBox.TabIndex = 1
+        Me.cboComboBox.Text = "Select Location"
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Stencil", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(5, 8)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(406, 33)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "Oceanic Scuba Expeditions"
+        '
+        'frmScuba
+        '
+        Me.AcceptButton = Me.btnDiveCost
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Scuba_Expedition.My.Resources.Resources.scuba
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.CancelButton = Me.btnClear
+        Me.ClientSize = New System.Drawing.Size(848, 434)
+        Me.Controls.Add(Me.pnlScuba)
+        Me.Name = "frmScuba"
+        Me.Text = "Scuba Expeditions"
+        Me.pnlScuba.ResumeLayout(False)
+        Me.pnlScuba.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pnlScuba As Panel
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents cboComboBox As ComboBox
+    Friend WithEvents lstDive As ListBox
+    Friend WithEvents lblSelect As Label
+    Friend WithEvents txtTeam As TextBox
+    Friend WithEvents lblTeam As Label
+    Friend WithEvents lblCost As Label
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnDiveCost As Button
+    Friend WithEvents lblLength As Label
+    Friend WithEvents lblScubaDive As Label
+End Class
---
+++ Scuba Expedition/frmScuba.vb
@@ -0,0 +1,238 @@
+﻿'Program Name: Scuba Expedition App
+'Developer: Jacqueline Gonzalez
+'Date: 01/30/2023
+'Purpose: The Scuba Expedition app determines the location and scuba dives available and
+'         calculates the total dive team cost.
+Public Class frmScuba
+
+    ' Class variables
+    Private _intFourHour As Integer = 4
+    Private _intSixHour As Integer = 6
+    Private _intEightFour As Integer = 8
+    Private _strDive1 As String = "Great Barrier Reef"
+    Private _strDive2 As String = "Tiger Shark"
+    Private _strDive3 As String = "Fathom Chasm"
+    Private _strDive4 As String = "Great Blue Chasm"
+    Private _strDive5 As String = "Rainbow Reef"
+    Private _strDive6 As String = "Namena Sunken Ship"
+
+    Private Sub cboComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboComboBox.SelectedIndexChanged
+        ' This event handler allows the user to enter the location choice and then calls subprocedures
+        ' to place the dive types in the list.
+        Dim intLocationChoice As Integer
+
+        intLocationChoice = cboComboBox.SelectedIndex
+        lstDive.Items.Clear()
+        Select Case intLocationChoice
+            Case 0
+                AustraliaTeam()
+            Case 1
+                BelizeTeam()
+            Case 2
+                FijiTeam()
+        End Select
+        ' Make items visible in the window
+        lblTeam.Visible = True
+        txtTeam.Visible = True
+        lblSelect.Visible = True
+        lstDive.Visible = True
+        btnDiveCost.Visible = True
+        btnClear.Visible = True
+        lblScubaDive.Visible = True
+        lblCost.Visible = True
+        lblLength.Visible = True
+        'Clear the labels
+        lblScubaDive.Text = ""
+        lblCost.Text = ""
+        lblLength.Text = ""
+        'Set focus on number in dive team text box
+        txtTeam.Focus()
+    End Sub
+    Private Sub AustraliaTeam()
+        ' This procedure fills in the possible dive types offered in Australia
+        lstDive.Items.Add(_strDive1)
+        lstDive.Items.Add(_strDive2)
+        lstDive.Items.Add(_strDive3)
+    End Sub
+    Private Sub BelizeTeam()
+        'This procedure fills in the possible dive types offered in Belize
+        lstDive.Items.Add(_strDive4)
+        lstDive.Items.Add(_strDive5)
+    End Sub
+    Private Sub FijiTeam()
+        ' This procedure fills in the possible dive types offered in Fiji
+        lstDive.Items.Add(_strDive2)
+        lstDive.Items.Add(_strDive3)
+        lstDive.Items.Add(_strDive6)
+    End Sub
+
+    Private Sub btnDiveCost_Click(sender As Object, e As EventArgs) Handles btnDiveCost.Click
+        ' This button event handler determines the cost of scuba expeditions
+
+        Dim IntTeamSize As Integer
+        Dim blnNumberInTeamIsValid As Boolean = False
+        Dim blnDiveIsSelected As Boolean = False
+        Dim intDiveChoice As Integer
+        Dim strSelectedTeam As String = ""
+        Dim intLocationChoice As Integer
+        Dim intLength As Integer = 0
+        Dim decTotalCost As Decimal
+
+        'Call a function to ensure the number of people in the dive team is valid
+        blnNumberInTeamIsValid = ValidateNumberInTeam()
+        'Call a function to ensure a scub dive was selected
+        intDiveChoice = ValidateDiveSelection(blnDiveIsSelected, strSelectedTeam)
+        ' If number of people and the dive trip are valid, calculate the ocst
+        If (blnNumberInTeamIsValid And blnDiveIsSelected) Then
+            IntTeamSize = Convert.ToInt32(txtTeam.Text)
+            intLocationChoice = cboComboBox.SelectedIndex
+            Select Case intLocationChoice
+                Case 0
+                    decTotalCost = AustraliaDiveCost(intDiveChoice, IntTeamSize, intLength)
+                Case 1
+                    decTotalCost = BelizeDiveCost(intDiveChoice, IntTeamSize, intLength)
+                Case 2
+                    decTotalCost = FijiDiveCost(intDiveChoice, IntTeamSize, intLength)
+            End Select
+            ' Display the cost of the scuba dive
+            lblScubaDive.Text = "Dive: " & strSelectedTeam
+            lblCost.Text = "Cost: " & decTotalCost.ToString("C")
+            lblLength.Text = "LEngth: " & intLength.ToString() & " hours"
+        End If
+    End Sub
+    Private Function ValidateNumberInTeam() As Boolean
+        ' This procedure validates the value entered for the dive team size
+        Dim intTeamSize As Integer
+        Dim blnValidityCheck As Boolean = False
+        Dim strNumberInTeamMessage As String = "Please enter the number of people in your dive team"
+        Dim strMessageBoxTitle As String = "Error"
+
+        Try
+            intTeamSize = Convert.ToInt32(txtTeam.Text)
+            If intTeamSize >= 2 And intTeamSize <= 6 Then
+                blnValidityCheck = True
+            Else
+                MsgBox(strNumberInTeamMessage, , strMessageBoxTitle)
+                txtTeam.Focus()
+                txtTeam.Clear()
+            End If
+        Catch Exception As FormatException
+            MsgBox(strNumberInTeamMessage, , strMessageBoxTitle)
+            txtTeam.Focus()
+            txtTeam.Clear()
+        Catch Exception As OverflowException
+            MsgBox(strNumberInTeamMessage, , strMessageBoxTitle)
+            txtTeam.Focus()
+            txtTeam.Clear()
+        Catch Exception As SystemException
+            MsgBox(strNumberInTeamMessage, , strMessageBoxTitle)
+            txtTeam.Focus()
+            txtTeam.Clear()
+        End Try
+        Return blnValidityCheck
+    End Function
+
+    Private Function ValidateDiveSelection(ByRef blnDive As Boolean, ByRef strDive As String) As Integer
+        ' This function esnures the user selected a dive type
+        Dim intDiveTyoe As Integer
+        Try
+            intDiveTyoe = Convert.ToInt32(lstDive.SelectedIndex)
+            strDive = lstDive.SelectedItem.ToString()
+            blnDive = True
+        Catch Exception As SystemException
+            ' Detects if a dive type is not selected
+            MsgBox("Select a Dive Type", , "Error")
+            blnDive = False
+        End Try
+        Return intDiveTyoe
+    End Function
+
+    Private Function AustraliaDiveCost(ByVal intDive As Integer, ByVal intTeam As Integer, ByRef intTime As Integer) As Decimal
+        ' This function calculates the cost of the dive team in Australia
+        Dim decTeamCost As Decimal
+        Dim decFinalCost As Decimal
+        Dim decAustraliaDive1 As Decimal = 359D
+        Dim decAustraliaDive2 As Decimal = 200D
+        Dim decAustraliaDive3 As Decimal = 125D
+
+        Select Case intDive
+            Case 0
+                decTeamCost = decAustraliaDive1
+                intTime = _intEightFour
+            Case 1
+                decTeamCost = decAustraliaDive2
+                intTime = _intSixHour
+            Case 2
+                decTeamCost = decAustraliaDive3
+                intTime = _intFourHour
+        End Select
+        decFinalCost = decTeamCost * intTeam
+        Return decFinalCost
+    End Function
+
+    Private Function BelizeDiveCost(ByVal intDive As Integer, ByVal intTeam As Integer, ByRef intTime As Integer) As Integer
+        ' This function calculates the cost of the dive team in Belize
+        Dim decTeamCost As Decimal
+        Dim decFinalCost As Decimal
+        Dim decBelizeDive4 As Decimal = 200D
+        Dim decBelizeDive5 As Decimal = 150D
+
+        Select Case intDive
+            Case 0
+                decTeamCost = decBelizeDive4
+                intTime = _intEightFour
+            Case 1
+                decTeamCost = decBelizeDive5
+                intTime = _intSixHour
+        End Select
+        decFinalCost = decTeamCost * intTeam
+        Return decFinalCost
+    End Function
+
+    Private Function FijiDiveCost(ByVal intDive As Integer, ByVal intTeam As Integer, ByRef intTime As Integer) As Decimal
+        ' This function calculates the cost of the dive team in Fiji
+        Dim decTeamCost As Decimal
+        Dim decFinalCost As Decimal
+        Dim decFijiDive2 As Decimal = 280D
+        Dim decFijiDive3 As Decimal = 225D
+        Dim decFijiDive6 As Decimal = 200D
+
+        Select Case intDive
+            Case 0
+                decTeamCost = decFijiDive2
+                intTime = _intEightFour
+            Case 1
+                decTeamCost = decFijiDive3
+                intTime = _intSixHour
+            Case 2
+                decTeamCost = decFijiDive6
+                intTime = _intFourHour
+        End Select
+        decFinalCost = decTeamCost * intTeam
+        Return decFinalCost
+    End Function
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' This event handler clears the form and resets the form for reuse
+        cboComboBox.Text = "Select Location"
+        txtTeam.Clear()
+        lstDive.Items.Clear()
+        lblScubaDive.Text = ""
+        lblCost.Text = ""
+        lblLength.Text = ""
+        lblTeam.Visible = False
+        txtTeam.Visible = False
+        lblSelect.Visible = False
+        lstDive.Visible = False
+        btnDiveCost.Visible = False
+        btnClear.Visible = False
+        lblScubaDive.Visible = False
+        lblCost.Visible = False
+        lblLength.Visible = False
+    End Sub
+
+    Private Sub frmScuba_Dive(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Hold the splash screen for 5 seconds
+        Threading.Thread.Sleep(5000)
+    End Sub
+End Class
